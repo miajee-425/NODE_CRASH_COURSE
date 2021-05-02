667aa1f9fa36deb5d50ab0ad227f12cc0d6ec0e4
miajee <miajee@sheba.xyz>
2021-05-02 12:36:46 +0600

    url Module

--- reference/url_demo.js
+++ reference/url_demo.js
@@ -1,4 +1,30 @@
-const os = require('os');
+const url = require('url');
 
-// Platform
-console.log(os.platform());
+const myUrl = new URL('http://mywebsite.com:8080/hello.html?id=100&status=active');
+
+// Serialized URL
+console.log(myUrl.href);
+console.log(myUrl.toString())
+
+// Host (root domain)
+console.log(myUrl.host);
+// Hostname (does not get port)
+console.log(myUrl.hostname) 
+
+// Pathname
+console.log(myUrl.pathname);
+
+// Serialized query
+console.log(myUrl.search)
+
+// params object
+console.log(myUrl.searchParams);
+
+// Add param
+myUrl.searchParams.append('abe', 123);
+console.log(myUrl.searchParams);
+
+// Loop through params
+myUrl.searchParams.forEach((value, key)=>{
+    console.log(`${key} : ${value}`);
+});
