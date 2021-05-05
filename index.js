ffdf99b5509286ac8c920f3fac7201df9c0eb65c
miajee <miajee@sheba.xyz>
2021-05-05 21:00:24 +0600

    Content-Type add, Nodemon add

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const path = require('path');
 const fs = require('fs');
 
 const server = http.createServer((req, res) => {
- console.log(req.url);
+    if(req.url === '/') {
+        res.writeHead(200, { 'Content-Type': 'text/html' })
+        res.end('<h1>Home</h1>');   
+    }
 });
 
 const PORT = process.env.PORT || 5000;
