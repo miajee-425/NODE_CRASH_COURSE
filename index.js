5c7caf90a8377866cf0fa3484de5a2f5ec9e8833
miajee <miajee@sheba.xyz>
2021-05-05 20:47:17 +0600

    Real life example of http Part 01

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-const Logger = require('./logger');
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
 
-const logger = new Logger();
-
-// to register listeners
-logger.on('message', (data)=> { 
-    console.log('Called Listener', data);
+const server = http.createServer((req, res) => {
+ console.log(req.url);
 });
 
-logger.log('Hello');
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => { console.log(`Server running on port ${PORT}`) });
