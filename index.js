3ab80458e631bafd524966c67cf55391a4d67488
miajee <miajee@sheba.xyz>
2021-05-05 21:17:37 +0600

    Add index.html and About.html

--- index.js
+++ index.js
@@ -4,10 +4,21 @@ const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     if(req.url === '/') {
-        res.writeHead(200, { 'Content-Type': 'text/html' })
-        res.end('<h1>Home</h1>');   
+        fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, data) => {
+            if(err) throw err;
+            res.writeHead(200, { 'Content-Type': 'text/html' })
+            res.end(data);
+        });   
     }
-});
+
+    if(req.url === '/about') {
+        fs.readFile(path.join(__dirname, 'public', 'about.html'), (err, data) => {
+            if(err) throw err;
+            res.writeHead(200, { 'Content-Type': 'text/html' })
+            res.end(data);
+        });   
+    }
+});  
 
 const PORT = process.env.PORT || 5000;
 
